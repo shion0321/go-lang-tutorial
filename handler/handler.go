a025cffbf5528fec92a336e2e4847fc444c5292c
Shion Aoki <shio0321pc@gmail.com>
2021-02-09 08:12:14 +0900

    add

--- handler/handler.go
+++ handler/handler.go
@@ -7,6 +7,7 @@ import (
 
 func MainPage() echo.HandlerFunc {
     return func(c echo.Context) error {     //c をいじって Request, Responseを色々する 
-        return c.String(http.StatusOK, "Hello World")
+			userId := c.Param("username")
+        return c.String(http.StatusOK, "Hello World" + userId)
     }
 }
